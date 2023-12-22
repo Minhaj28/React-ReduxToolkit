d91de27113a5f142f4e6bdb806c7e0e6c8962c0c
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-12-23 00:56:40 +0600

    add views , store and slice

---
+++ src/features/icecreame/IcecreameView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IcecreamView = () => {
+  return (
+    <div>
+        <h2>Number of Ice Creams - </h2>
+        <button>Order Ice cream</button>
+        <button>Restock Ice cream</button>
+    </div>
+  )
+}
